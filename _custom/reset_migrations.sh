e55628c8a7aa5d20b780040d4508882687d18e23
Rakshith Aloori <rakshithaloori10sa@gmail.com>
2023-09-25 12:42:55 +0530

    reset migrations

---
+++ _custom/reset_migrations.sh
@@ -0,0 +1,6 @@
+find . -path "*/migrations/*.py" -not -name "__init__.py" -delete
+find . -path "*/migrations/*.pyc"  -delete
+rm db.sqlite3
+python manage.py makemigrations
+python manage.py migrate
+python manage.py createsuperuser
